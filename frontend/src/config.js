e338a9542606b4ab63572d6cff1557392f4ad564
asher603 <aabensou@g.jct.ac.il>
2025-12-13 19:26:10 +0200

    fixed to access the url from render instead of localhost

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 // Currently pointing to localhost for development.
 // Once you receive the URL from Render, replace the string here.
 
-export const API_URL = "http://localhost:4000";
+export const API_URL = "https://wedding-planner-api-s9so.onrender.com";
 
 // For example, in the future it will look like this:
 // export const API_URL = "https://wedding-planner-api.onrender.com";
