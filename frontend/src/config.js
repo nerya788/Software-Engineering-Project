8dcdfd0b42a813453853f229c3f71f54b6837fdb
asher603 <aabensou@g.jct.ac.il>
2025-12-24 16:02:55 +0200

    refactor: make API_URL dynamic for dev/prod

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,17 @@
 // Main configuration file
 
-export const API_URL = "https://wedding-planner-api-s9so.onrender.com";
+// Vite automatically detects if the app is running in Production (Build) or Development (Dev) mode
+
+export const API_URL = import.meta.env.PROD 
+  ? "https://wedding-planner-api-s9so.onrender.com" // Production URL (Render)
+  : "http://localhost:4000"; // Development URL (Localhost)
+
+/** 
+Notes:
+
+1. To run locally: Make sure the backend server is running on port 4000 
+(if not, run "npm run dev" in the backend folder to start it), 
+Then run "npm run dev" in the frontend folder AND 
+
+2. To run on Render: Use the client link provided by Render.
+*/
